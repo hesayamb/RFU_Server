50901043c3c5bcd63046f63a02e83a01d69e9a31
vikmanU <vikman@vikman.vikmanD>
2023-01-07 06:10:11 -0500

    Log system implemented

---
+++ src/common/log.h
@@ -0,0 +1,87 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <time.h>
+#include <string.h>
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+// === auxiliar functions
+static inline char *timenow();
+
+#define _FILE strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
+
+#define NO_LOG          0x00
+#define ERROR_LEVEL     0x01
+#define INFO_LEVEL      0x02
+#define DEBUG_LEVEL     0x03
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL   DEBUG_LEVEL
+#endif
+
+#define PRINTFUNCTION(format, ...)      fprintf(stderr, format, __VA_ARGS__)
+
+#define LOG_FMT             "%s | %-7s | %-15s | %s:%d | "
+#define LOG_ARGS(LOG_TAG)   timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
+
+#define NEWLINE     "\n"
+
+#define ERROR_TAG  BOLDRED "ERROR" RESET
+#define INFO_TAG   BOLDWHITE "INFO" RESET
+#define DEBUG_TAG  BOLDBLUE "DEBUG" RESET
+
+#if LOG_LEVEL >= DEBUG_LEVEL
+#define LOG_DEBUG(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ## args)
+#else
+#define LOG_DEBUG(message, args...)
+#endif
+
+#if LOG_LEVEL >= INFO_LEVEL
+#define LOG_INFO(message, args...)      PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## args)
+#else
+#define LOG_INFO(message, args...)
+#endif
+
+#if LOG_LEVEL >= ERROR_LEVEL
+#define LOG_ERROR(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#else
+#define LOG_ERROR(message, args...)
+#endif
+
+#if LOG_LEVEL >= NO_LOGS
+#define LOG_IF_ERROR(condition, message, args...) if (condition) PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#else
+#define LOG_IF_ERROR(condition, message, args...)
+#endif
+
+static inline char *timenow() {
+    static char buffer[64];
+    time_t rawtime;
+    struct tm *timeinfo;
+    
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    
+    strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", timeinfo);
+    
+    return buffer;
+}
+
+#endif //LOG_H
